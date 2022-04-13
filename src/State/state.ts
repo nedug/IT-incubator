db2773dd8e413a1f55690740d5ad772205219b47
nedug2 <80103497+nedug@users.noreply.github.com>
2022-04-13 15:32:15 +0300

    redux dev tools

--- src/State/state.ts
+++ src/State/state.ts
@@ -1,6 +1,7 @@
 import {combineReducers, createStore} from "redux";
 import {tasksReducer} from "./tasksReducer";
 import {todoListReducer} from "./todoListReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
@@ -11,7 +12,7 @@ const rootReducer = combineReducers({
 });
 
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
 
 
 // @ts-ignore
