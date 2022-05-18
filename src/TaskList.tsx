04edacba3211c89591ad37774e4375741f9cc2c7
Alexander R <ru5nedug@mail.ru>
2022-05-18 16:04:30 +0300

    Update TaskList.tsx

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -21,10 +21,10 @@ const TaskList = React.memo(({todolist,}: TaskListPropsType) => {
 
     console.log('TaskList')
 
-    const tasks = useSelector<AppRootStateType, Array<TaskType>>(state => {
+    const tasks = useSelector<AppRootStateType, Array<TaskType>>(state => state.tasks[todolist.id]);
 
-        const tasks = state.tasks[todolist.id];
 
+    const getFilteredTaskForRender = () => {
         switch (todolist.filter) {
             case SortedTask.active:
                 return tasks.filter(task => !task.isDone);
@@ -33,7 +33,7 @@ const TaskList = React.memo(({todolist,}: TaskListPropsType) => {
             default:
                 return tasks;
         }
-    });
+    }
 
 
     return (
@@ -41,7 +41,7 @@ const TaskList = React.memo(({todolist,}: TaskListPropsType) => {
             ?
             <div style={{margin: '10px 0'}}>
                 {
-                    tasks.map(task => (
+                    getFilteredTaskForRender().map(task => (
                         <Task key={task.id}
                               {...task}
                               todolistId={todolist.id}
