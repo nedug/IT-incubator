cd00021476bda7394d3194f262f1d11479d956ee
Alexander R <ru5nedug@mail.ru>
2022-07-14 15:04:06 +0300

    scroll todo

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -2,6 +2,7 @@ import React, { CSSProperties } from 'react';
 import Task from './Task';
 import { useAppSelector } from './State/store';
 import { SortedTask, TodoListCommonType } from './State/todolist-reducer';
+import s from './CSS/TaskList.module.css';
 
 type TaskListPropsType = {
     todolist: TodoListCommonType
@@ -11,7 +12,7 @@ const EmptyListStyle: CSSProperties = {
     fontSize: 'small',
     color: '#858585',
     padding: '10px',
-}
+};
 
 
 const TaskList = React.memo(({ todolist }: TaskListPropsType) => {
@@ -27,13 +28,20 @@ const TaskList = React.memo(({ todolist }: TaskListPropsType) => {
             default:
                 return tasks;
         }
-    }
+    };
 
 
     return (
         tasks.length
             ?
-            <div style={{ margin: '10px 0' }}>
+            <div
+                className={s.scroll}
+                style={{
+                    margin: '10px 0',
+                    height: getFilteredTaskForRender().length > 4 ? '152px' : 'auto',
+                    overflow: 'auto',
+                }}
+            >
                 {
                     getFilteredTaskForRender().map(task => (
                         <Task key={task.id}
